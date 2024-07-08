196b6eba42ce71b6abf282316185abf989aaa1bf
abdelach <abdou.achakaj@gmail.com>
2024-07-08 23:24:30 +0100

    first com

---
+++ Backend/public/wp-content/plugins/all-bootstrap-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-polyfill', 'wp-primitives', 'wp-url', 'wp-viewport'), 'version' => '0f9496b5aed13b5c82bc2b301821f9d5');
